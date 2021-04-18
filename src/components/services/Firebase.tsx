33e7cdd9a6df2abe72ebe598597734aebf26b502
tudorale <tudorale2005@gmail.com>
2021-04-17 20:39:48 -1200

    first version of JustType

--- src/components/services/Firebase.tsx
+++ src/components/services/Firebase.tsx
@@ -2,9 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/storage";
 
-
 const firebaseConfig = {
-   // not allowed for you
+   apiKey: "AIzaSyBPuJnN5gI3o_YZeVNlZwOky-7Y6LDGrTI",
+   authDomain: "justtype-preview.firebaseapp.com",
+   projectId: "justtype-preview",
+   storageBucket: "justtype-preview.appspot.com",
+   messagingSenderId: "648878251377",
+   appId: "1:648878251377:web:e42775dc14919b1475ead0"
 };
 
 // Initialize Firebase
